3e0a029ce4df646bb0cc4ec8642aafe39964826d
Noah Lanson <lanson.noah@gmail.com>
2021-05-29 23:01:44 +1000

    Anim working. Letter spacing not.

--- src/Main.ts
+++ src/Main.ts
@@ -1,4 +1,20 @@
-import { AceAttorneyImage, AceAttorneyTextBox, FlashingRects } from './Render';
+import { RenderGIF } from './Render';
 
 
-var b = new FlashingRects(100,100);
+var b = new RenderGIF(1200, 620, 50);
+b.startEncoder('hello');
+
+b.fillBackground('yellow');
+
+const text = 'Hello!';
+var prevWidth: number = 0;
+var gap: number = 2;
+
+//Need to figure out how to have constant width between characters.
+for ( let i=0; i<text.length; i++ ) {
+    b.writeText(text[i], 400+prevWidth, 310);
+    prevWidth += b.measureText(text[i])
+    b.setFrame();
+}
+
+b.finishEncodig();
--- src/Render.ts
+++ src/Render.ts
@@ -3,161 +3,87 @@ import GIFEncoder from 'gifencoder';
 import * as fs from 'fs';
 
 
-//Doesn't work (produces transparent blank)
-export class AceAttorneyImage {
-    private canvas = createCanvas(200,200);
-    private ctx = this.canvas.getContext('2d');
+//Create an image, add background and write text.
+export class RenderImage {
 
-    constructor(
-        private text: string
-    ) { }
-
-    public drawBackground(color: string):void {
-        this.ctx.fillStyle = color;
-        this.ctx.fillRect(0, 0, 200, 200);
-    }
-
-    public async loadImage() {
-        let image = await loadImage('assets/text-box.png');
-        this.ctx.drawImage(image, 50, 0, 100, 100);
-        console.log('Image drawn');
-    }
-
-    public save(name: string) {
-        const buffer = this.canvas.toBuffer("image/png");
-        fs.writeFileSync(`./out/${name}.png`, buffer);
-    }
-
-}
-
-
-//Doesn't work (producing white image with grey line)
-export class AceAttorneyTextBox {
-    private encoder: GIFEncoder;
     private canvas: any;
     private ctx: any;
     
     constructor(
-        private text: string,
-        private char: string,
-        private readonly x: number,
-        private readonly y: number,
-        private outfile: string
+        private width: number,
+        private height: number
     ) {
-        this.encoder = new GIFEncoder(x, y);
-        this.canvas = createCanvas(x, y);
+        this.canvas = createCanvas(this.width, this.height);
         this.ctx = this.canvas.getContext('2d');
-
-        this.setDefaultEncoderProperties();
-        this.startEncoder();
-        //this.addBackground(`white`);
-        this.loadAceAttorneyTextBox();
-        this.draw();
-        this.stopEncoder();
     }
 
-    private setDefaultEncoderProperties() {
-        this.encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat
-        this.encoder.setDelay(100);  // frame delay in ms
-        this.encoder.setQuality(10); // image quality. 10 is default.
+    public save(name: string) {
+        const buffer = this.canvas.toBuffer('image/png');
+        fs.writeFileSync(`./out/${name}.png`, buffer);
     }
 
-    private addBackground(colour: string) {
+    public fillBackground(colour: string) {
         this.ctx.fillStyle = colour;
-        this.ctx.fillRect(0,0,200,200);
+        this.ctx.fillRect(0, 0, this.width, this.height);
     }
 
-    private loadAceAttorneyTextBox() {
-        // Draw cat with lime helmet
-        loadImage('assets/text-box.png').then((image) => {
-            this.ctx.drawImage(image, 50, 0, 70, 70)
-        
-            console.log('<img src="' + this.canvas.toDataURL() + '" />')
-        });
-    }
-
-    private startEncoder() {
-        this.encoder.createReadStream().pipe(fs.createWriteStream(`./out/${this.outfile}.gif`));
-        this.encoder.start();
-    }
-
-    private draw() {
-        const write = () => {
-            //Write text
-            this.ctx.font = '20px impact';
-            this.ctx.fillText(this.text, 50, 100);
-        }
-
-        const drawLines = () => {
-            //Draw line under text
-            var mtext = this.ctx.measureText(this.text);
-            this.ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-            this.ctx.beginPath();
-            this.ctx.lineTo(50,102);
-            this.ctx.lineTo(50 + mtext.width, 102);
-            this.ctx.stroke();
-        }
-
-        const setFrame = () => {
-            this.encoder.addFrame(this.ctx);
-        }
-
-        //No loop. Just a simple write
-        write();
-        drawLines();
-        setFrame();
-    }
-
-    private stopEncoder() {
-        this.encoder.finish();
+    public writeText(text: string) {
+        this.ctx.fillStyle = "#000";
+        this.ctx.font = "72px Arial";
+        this.ctx.textAlign = "center";
+        this.ctx.fillText(text, 400, 120);
     }
 
 }
 
-
-//Produces a rainbow rgb rectangle.
-//Change incremental values in drawFrames() method to change how many colours are rendered.
-//More incrementations = more colours and more render time.
-export class FlashingRects {
+//Class to help create text gif.
+//See Main.ts for usage.
+//Help from https://sebhastian.com/node-canvas/
+export class RenderGIF {
 
     private canvas: any;
     private ctx: any;
     private encoder: GIFEncoder;
 
     constructor(
-        private readonly x: number,
-        private readonly y: number
-    ) { 
-        this.canvas = createCanvas(x, y);
+        private width: number,
+        private height: number,
+        private timeDelay: number
+    ) {
+        this.canvas = createCanvas(width, height);
         this.ctx = this.canvas.getContext('2d');
-        this.encoder = new GIFEncoder(x, y);
-
-        this.startEncoder();
-        this.drawFrames();
-        this.endEncoder();
+        this.encoder = new GIFEncoder(width, height);
     }
 
-    private startEncoder() {
-        this.encoder.createReadStream().pipe(fs.createWriteStream('out/rgb.gif'));
+    public startEncoder(name: string) {
+        this.encoder.createReadStream().pipe(fs.createWriteStream(`out/${name}.gif`));
         this.encoder.start();
-        this.encoder.setRepeat(0);   //0 for repeat, -1 for no-repeat.
-        this.encoder.setDelay(1);  //Frame delay in ms.
-        this.encoder.setQuality(1); //Image quality.
+        this.encoder.setRepeat(-1);   //0 for repeat, -1 for no-repeat.
+        this.encoder.setDelay(this.timeDelay);    //Frame delay in ms.
+        this.encoder.setQuality(10);  //Image quality.
     }
 
-    private drawFrames() {
-        for ( let red=0; red<256; red = red + 50 ) {
-            for ( let green=0; green<256; green = green + 50 ){
-                for ( let blue=0; blue<256; blue = blue + 50 ) {
-                    this.ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
-                    this.ctx.fillRect(0, 0, this.x, this.y);
-                    this.encoder.addFrame(this.ctx);
-                }
-            }
-        }
-    }
-
-    private endEncoder() {
+    public finishEncodig() {
         this.encoder.finish();
+    }
+
+    public setFrame() {
+        this.encoder.addFrame(this.ctx);
+    }
+
+    public fillBackground(colour: string) {
+        this.ctx.fillStyle = colour;
+        this.ctx.fillRect(0, 0, this.width, this.height);
+    }
+
+    public writeText(text: string, x: number, y: number) {
+        this.ctx.fillStyle = "#000";
+        this.ctx.font = "72px Arial";
+        this.ctx.textAlign = "center";
+        this.ctx.fillText(text, x, y);
+    }
+
+    public measureText(text: string): number {
+        return this.ctx.measureText(text).width;
     }
 }
