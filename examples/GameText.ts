0cc2412a1a8a427934e1b71d9339a093ec511454
nlanson <lanson.noah@gmail.com>
2021-06-01 15:38:31 +1000

    GameText example

---
+++ examples/GameText.ts
@@ -0,0 +1,25 @@
+//This example generates a GIF with text scrolling ontop of an image.
+
+import { ScrollingTextOnImage } from '../src/Render';
+
+const a = async () => {
+    var b = new ScrollingTextOnImage(
+        1000,  //Outut width
+        750    //Output height
+    );
+
+
+    b.encoderOptions(50, false);    //Set encoder options.
+    b.startEncoder('hello-world');  //Start encoder and set output name. (Required)
+    b.fillBackground('white');      //Fill image background with white. (Optional)
+    b.setWriteStyle('#fff', '32px Arial', 'center'); //Set text style. (For best output)
+
+    await b.loadImage('text-box.png', -10, 0);   //Load image onto canvas. (Can also be used to load a background image)
+    b.addScrollingText('Hello World!', 10, 575); //Add scrolling text ontop of image.
+
+
+    b.finishEncoding();  //Finish encoding.
+}
+
+
+a();
