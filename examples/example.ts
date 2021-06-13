ff56ba55cdfa693a2f37ba4d0ed2cffa18de92cf
Noah Lanson <lanson.noah@gmail.com>
2021-06-13 14:14:55 +1000

    remove chaining feature

--- examples/example.ts
+++ examples/example.ts
@@ -1,6 +1,5 @@
 import { 
     CourtScene,
-    CourtProceeding,
     PheonixActions,
     JudgeActions,
     EdgeworthActions
@@ -22,28 +21,3 @@ const a = async (): Promise<void> => {
 
 
 a();
-
-//Chaining court scenes.
-const b = async (): Promise<void> => {
-    var b = new CourtProceeding([
-        {
-        character: 'judge',
-        displayName: 'Justice',
-        background: 'judge',
-        dialog: "How are you?",
-        action: JudgeActions.EYES_CLOSED
-        },
-        {
-            character: 'pheonix',
-            displayName: 'nlanson',
-            background: 'defence',
-            dialog: "Very good thanks",
-            action: PheonixActions.READ
-        },
-    ]);
-
-    //await b.renderScenes();
-    b.chainGifs();
-}
-
-//b();
